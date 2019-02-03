b67075c50ed33c344b54e8d64772bc90ecbf4066
Nathan Figueroa <figuernd@miamioh.edu>
2019-02-03 15:26:04 -0500

    Added self reporting to test suite

--- tests/lab5.js
+++ tests/lab5.js
@@ -1,3 +1,32 @@
+let LabData = {
+  LAB_ID: "lab5",
+  failCount: 0,
+  passCount: 0,
+  reporter = {
+    specDone: function(result) {
+      LabData.failCount += result.failedExpectations.length;
+      LabData.passCount += result.passedExpectations.length;
+    },
+    jasmineDone: function(info) {
+      const endpoint = 'https://na9gsijrt3.execute-api.us-east-1.amazonaws.com/deployed/result';
+      const body = {
+        "lab": LabData.LAB_ID,
+        "name": StudentName ? StudentName : "Unknown",
+        "testsPassed": LabData.passCount,
+        "testsTotal": LabData.passCount + LabData.failCount
+      };
+      const params = {
+        method: 'POST',
+        mode: 'cors',
+        body: body
+      };
+      fetch(endpoint, params)
+        .catch(err => {console.log(err)});
+    }
+  }
+};
+jasmine.getEnv().addReporter(LabData.reporter);
+
 
 describe("hello", function() {
   it('Returns "hello"', function() {
@@ -62,3 +91,5 @@ describe("farm object", function() {
     expect(farm.house.dogs.food).toEqual('dog chow');
   });
 });
+
+
